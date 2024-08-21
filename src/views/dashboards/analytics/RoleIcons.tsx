5b53f4a76f7a4570093a56aad06bfe9e76cebc8c
RobinaShaheen <robina1961739@gmail.com>
2024-08-21 15:49:37 +0500

    Initial commit

--- src/views/dashboards/analytics/RoleIcons.tsx
+++ src/views/dashboards/analytics/RoleIcons.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid, Box } from '@mui/material';
 import { Person as PersonIcon } from '@mui/icons-material';
+import Link from 'next/link';
 
 const RolesList: React.FC = () => {
   const roles = [
-    { role: 'Admin', count: 1 },
-    { role: 'Accountant', count: 1 },
-    { role: 'Doctor', count: 4 },
-    { role: 'Pharmacist', count: 1 },
-    { role: 'Pathologist', count: 1 },
-    { role: 'Radiologist', count: 1 },
-    { role: 'Super Admin', count: 1 },
-    { role: 'Receptionist', count: 1 },
-    { role: 'Nurse', count: 2 },
+    { role: 'Admin', count: 1, path: '/human-resource' },
+    { role: 'Accountant', count: 1, path: '/human-resource'  },
+    { role: 'Doctor', count: 4, path: '/human-resource'  },
+    { role: 'Pharmacist', count: 1, path: '/human-resource'  },
+    { role: 'Pathologist', count: 1, path: '/human-resource'  },
+    { role: 'Radiologist', count: 1, path: '/human-resource'  },
+    { role: 'Super Admin', count: 1, path: '/human-resource'  },
+    { role: 'Receptionist', count: 1, path: '/human-resource'  },
+    { role: 'Nurse', count: 2, path: '/human-resource'  },
   ];
 
   return (
     <Box mt={4}>
       {roles.map((roleData, index) => (
-        <Card key={index} variant="outlined" style={{ marginBottom: 16 }}>
+        <Card key={index} variant="outlined" style={{ marginBottom: 16}} 
+          sx={{'&:hover': {boxShadow: '0 10px 30px rgba(0, 0, 0, 0.3)'}}}
+          >
+          <Link href={roleData.path} style={{textDecoration: 'none'}}>
           <CardContent>
-            <Grid container alignItems="center" spacing={2}>
+            <Grid container alignItems="center" spacing={2} sx={{cursor: 'pointer'}}>
               <Grid item>
                 <Box
                   style={{
@@ -41,6 +45,7 @@ const RolesList: React.FC = () => {
               </Grid>
             </Grid>
           </CardContent>
+          </Link>
         </Card>
       ))}
     </Box>
