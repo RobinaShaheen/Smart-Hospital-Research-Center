0e880022cac8f36dd196a8a69689dbb20e49f753
Nabila Shaheen <nabila1961739@gmail.com>
2024-08-21 15:41:05 +0500

    initial commit

--- src/views/dashboards/analytics/IncomeExpenseCards.tsx
+++ src/views/dashboards/analytics/IncomeExpenseCards.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
 import { Box, Card, CardContent, Typography } from '@mui/material'
 import { MoneyOff, LocalHospital, LocalPharmacy, MedicalServices, LocalLibrary, MedicalInformation, DirectionsCar, InsertChart } from '@mui/icons-material'
+import { shadows } from '@mui/system'
 
 const incomeExpenseData = [
-  { title: 'OPD Income', amount: '$11,700.00', icon: <MedicalServices /> },
-  { title: 'IPD Income', amount: '$3,250.00', icon: <LocalHospital /> },
-  { title: 'Pharmacy Income', amount: '$2,626.40', icon: <LocalPharmacy /> },
-  { title: 'Pathology Income', amount: '$1,400.00', icon: <MedicalInformation /> },
-  { title: 'Radiology Income', amount: '$1,636.00', icon: <InsertChart /> },
-  { title: 'Blood Bank Income', amount: '$1,790.80', icon: <LocalLibrary /> },
-  { title: 'Ambulance Income', amount: '$1,521.00', icon: <DirectionsCar /> },
-  { title: 'General Income', amount: '$1,550.00', icon: <MoneyOff /> },
-  { title: 'Expenses', amount: '$225,800.00', icon: <MoneyOff /> }
+  { title: 'OPD Income', amount: '$11,700.00', icon: <MedicalServices />, path:'/Patients/OPD' },
+  { title: 'IPD Income', amount: '$3,250.00', icon: <LocalHospital /> , path:'/Patients/IPD'},
+  { title: 'Pharmacy Income', amount: '$2,626.40', icon: <LocalPharmacy />, path:'/pharmacy' },
+  { title: 'Pathology Income', amount: '$1,400.00', icon: <MedicalInformation />, path:'/pathology' },
+  { title: 'Radiology Income', amount: '$1,636.00', icon: <InsertChart />, path:'/radiology' },
+  { title: 'Blood Bank Income', amount: '$1,790.80', icon: <LocalLibrary />, path:'' },
+  { title: 'Ambulance Income', amount: '$1,521.00', icon: <DirectionsCar />, path:'' },
+  { title: 'General Income', amount: '$1,550.00', icon: <MoneyOff />, path:'/Finance/Income' },
+  { title: 'Expenses', amount: '$225,800.00', icon: <MoneyOff />, path:'/Finance/Expense' }
 ]
 
 const IncomeExpenseCards = () => {
@@ -25,16 +27,18 @@ const IncomeExpenseCards = () => {
       }}
     >
       {incomeExpenseData.map((item, index) => (
-        <Card key={index} sx={{ display: 'flex', alignItems: 'center', padding: 2 }}>
+        <Card key={index} sx={{ display: 'flex', alignItems: 'center', padding: 2 , '&:hover':{boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.42)'}}}>
+        <Link  href={item.path} style={{textDecoration:'none', width:'100%',}} >
           <CardContent sx={{ flex: 1, display: 'flex', alignItems: 'center', gap: 2 }}>
             <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: 40, height: 40, backgroundColor: '#F5F5F5', borderRadius: '50%' }}>
               {item.icon}
             </Box>
-            <Box>
+            <Box >
               <Typography variant='body1'>{item.title}</Typography>
               <Typography variant='h6'>{item.amount}</Typography>
             </Box>
           </CardContent>
+        </Link>
         </Card>
       ))}
     </Box>
